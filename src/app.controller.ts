0d7590c2022d6eaa388503e70e0818b97e8b1895
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-08 17:09:15 +0630

    learn connect microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'test_ms' })
+  getHello(@Payload() data: string): string {
+    console.log('ok na sa', data);
+    return this.appService.getHello(data);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 3003,
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(3004).then(() => {
+    console.log('Auth Service is Successfully started 3004');
+  });
 }
 bootstrap();
