8506367945b57e338d5b42de3fdf215e066bc2a6
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-09 17:03:27 +0630

    understand redis module using custom provider

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { createClient } from 'redis';
 
 @Module({
+  /** used custom provider (value provider) => provide can use any name you like */
   providers: [
     {
       provide: 'REDIS_OPTIONS',
@@ -9,6 +10,7 @@ import { createClient } from 'redis';
         url: 'redis://default:LbE3XSIud2xa1ypOgrYWs8yOKfOPZvTw@redis-18903.c292.ap-southeast-1-1.ec2.cloud.redislabs.com:18903',
       },
     },
+    /** used custom provider (factory provider) => [provide can use any name you like] / [inject is come from provide of value provider , and useFactory(callback function) take optional argument that is come from useValue key of value provider ]  */
     {
       inject: ['REDIS_OPTIONS'],
       provide: 'REDIS_CLIENT',
