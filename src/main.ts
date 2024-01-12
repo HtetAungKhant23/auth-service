a1e5d2f2d2271f257c9993670d3b8200fd40e78d
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-12 14:18:19 +0630

    test: using REDIS transport

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    // options: {
-    //   port: 3001,
-    // },
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
   });
   await app.startAllMicroservices();
   await app.listen(3001).then(() => {
