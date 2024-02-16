e71148075846c93e9579071742e62322749cacbe
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-02-16 11:07:57 +0630

    rmv console log in custom rpc exception

--- src/libs/exception/custom-rpc-exception.ts
+++ src/libs/exception/custom-rpc-exception.ts
@@ -2,12 +2,10 @@ import { RpcException } from '@nestjs/microservices';
 
 export class CustomRpcException extends RpcException {
   constructor(statusCode: number, message: string, devMessage: string) {
-    console.log(message, devMessage);
-
     super({
       statusCode: statusCode,
       error: statusCode >= 200 && statusCode <= 300 ? false : true,
-      messageEn: message,
+      message: message,
       devMessage: devMessage,
       body: null,
     });
